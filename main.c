763dc649c04fed9abd598d3c6dde36e06839477f
Nickolay Borschev <myonetelephon@gmail.com>
2024-12-16 04:18:29 +0300

    .c

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
-int main(void)
+#include <stdio.h>
+int main(void) 
 {
+    int ans = 0;
+    char element;
+    FILE *input = NULL;
+    input = fopen("data.txt", "r");
+    if (input == NULL) {
+        printf("error\n");
+        return -1;
+    }
+    while(fscanf(input, "%c", &element) != EOF) {
+        if (element == '(') {
+            ans += 1;
+        }
+        if (element == ')') {
+            ans -= 1;
+        }
+    }
+    printf("%d\n", ans);
+    fclose(input);
     return 0;
 }
+
